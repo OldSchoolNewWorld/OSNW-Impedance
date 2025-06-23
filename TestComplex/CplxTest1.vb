a8beef59436387c578f0a219a07295e032ad70ea
OldSchoolNewWorld <dreid02893@gmail.com>
2025-06-23 13:45:06 -0400

    Add tests of numeric format.

--- TestComplex/CplxTest1.vb
+++ TestComplex/CplxTest1.vb
@@ -7,7 +7,7 @@ Imports OSNW.Numerics.ComplexExtensions
 
 Namespace TestComplex
 
-    Public Class SignedCplxTest
+    Public Class CplxSignedTest
 
         <Fact>
         Sub ToStandardString_DefaultPosPos_Succeeds()
@@ -36,6 +36,40 @@ Namespace TestComplex
             Dim CplxStr As String = Z.ToStandardString()
             Assert.Equal("-1.125-i5.675", CplxStr)
         End Sub
+
+    End Class
+
+    Public Class CplxFormatTest
+
+        <Fact>
+        Sub ToStandardString_FormatPosPos_Succeeds()
+            ' One round down, one up.
+            Dim Z As New System.Numerics.Complex(1.122, 5.677)
+            Dim CplxStr As String = Z.ToStandardString(Nothing, "F2")
+            Assert.Equal("1.12+i5.68", CplxStr)
+        End Sub
+
+        <Fact>
+        Sub ToStandardString_DefaultPosNeg_Succeeds()
+            ' One round down, one up.
+            Dim Z As New System.Numerics.Complex(111_111.122, -555_555.677)
+            Dim CplxStr As String = Z.ToStandardString(Nothing, "N2")
+            Assert.Equal("111,111.12-i555,555.68", CplxStr)
+        End Sub
+
+        <Fact>
+        Sub ToStandardString_DefaultNegPos_Succeeds()
+            Dim Z As New System.Numerics.Complex(-111_111.125, 555_555.675)
+            Dim CplxStr As String = Z.ToStandardString(Nothing, "G5")
+            Assert.Equal("-1.1111E+05+i5.5556E+05", CplxStr)
+        End Sub
+
+        '<Fact>
+        'Sub ToStandardString_DefaultNegNeg_Succeeds()
+        '    Dim Z As New System.Numerics.Complex(-1.125, -5.675)
+        '    Dim CplxStr As String = Z.ToStandardString("F2", "N2", "G5")
+        '    Assert.Equal("-1.125-i5.675", CplxStr)
+        'End Sub
 
     End Class
 
--- TestImpedance/ImpdTest1.vb
+++ TestImpedance/ImpdTest1.vb
@@ -1,9 +1,10 @@
 Imports System
+Imports OSNW.Numerics
 Imports Xunit
 
 Namespace TestImpedance
 
-    Public Class SignedToStringTest
+    Public Class ToStringSignedTest
 
         <Fact>
         Sub ToString_DefaultPosPos_Succeeds()
@@ -33,9 +34,9 @@ Namespace TestImpedance
             Assert.Equal("<-1.125; -5.675>", ImpStr)
         End Sub
 
-    End Class ' SignedToStringTest
+    End Class ' ToStringSignedTest
 
-    Public Class SignedToStandardStringTest
+    Public Class ToStandardStringSignedTest
 
         <Fact>
         Sub ToStandardString_DefaultPosPos_Succeeds()
@@ -65,6 +66,40 @@ Namespace TestImpedance
             Assert.Equal("-1.125-j5.675", ImpStr)
         End Sub
 
-    End Class ' SignedToStandardStringTest
+    End Class ' ToStandardStringSignedTest
+
+    Public Class ToStandardStringFormatTest
+
+        <Fact>
+        Sub ToStandardString_FormatPosPos_Succeeds()
+            ' One round down, one up.
+            Dim Z As New OSNW.Numerics.Impedance(1.122, 5.677)
+            Dim CplxStr As String = Z.ToStandardString(Nothing, "F2")
+            Assert.Equal("1.12+j5.68", CplxStr)
+        End Sub
+
+        <Fact>
+        Sub ToStandardString_DefaultPosNeg_Succeeds()
+            ' One round down, one up.
+            Dim Z As New OSNW.Numerics.Impedance(111_111.122, -555_555.677)
+            Dim CplxStr As String = Z.ToStandardString(Nothing, "N2")
+            Assert.Equal("111,111.12-j555,555.68", CplxStr)
+        End Sub
+
+        <Fact>
+        Sub ToStandardString_DefaultNegPos_Succeeds()
+            Dim Z As New OSNW.Numerics.Impedance(-111_111.125, 555_555.675)
+            Dim CplxStr As String = Z.ToStandardString(Nothing, "G5")
+            Assert.Equal("-1.1111E+05+j5.5556E+05", CplxStr)
+        End Sub
+
+        '<Fact>
+        'Sub ToStandardString_DefaultNegNeg_Succeeds()
+        '    Dim Z As New OSNW.Numerics.Impedance(-1.125, -5.675)
+        '    Dim CplxStr As String = Z.ToStandardString("F2", "N2", "G5")
+        '    Assert.Equal("-1.125-j5.675", CplxStr)
+        'End Sub
+
+    End Class ' ToStandardStringFormatTest
 
 End Namespace ' TestImpedance
