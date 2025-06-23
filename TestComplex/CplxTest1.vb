6cf1f178bbbce5dd1a6260bca968709562c7fac6
OldSchoolNewWorld <dreid02893@gmail.com>
2025-06-23 08:54:58 -0400

    Implement and test basic ToStandardString() in Complex.

--- TestComplex/CplxTest1.vb
+++ TestComplex/CplxTest1.vb
@@ -10,28 +10,28 @@ Namespace TestComplex
     Public Class SignedCplxTest
 
         <Fact>
-        Sub ToStandardString_PosPos_Succeeds()
+        Sub ToStandardString_DefaultPosPos_Succeeds()
             Dim Z As New System.Numerics.Complex(1.125, 5.675)
             Dim CplxStr As String = Z.ToStandardString()
             Assert.Equal("1.125+i5.675", CplxStr)
         End Sub
 
         <Fact>
-        Sub ToStandardString_PosNeg_Succeeds()
+        Sub ToStandardString_DefaultPosNeg_Succeeds()
             Dim Z As New System.Numerics.Complex(1.125, -5.675)
             Dim CplxStr As String = Z.ToStandardString()
             Assert.Equal("1.125-i5.675", CplxStr)
         End Sub
 
         <Fact>
-        Sub ToStandardString_NegPos_Succeeds()
+        Sub ToStandardString_DefaultNegPos_Succeeds()
             Dim Z As New System.Numerics.Complex(-1.125, 5.675)
             Dim CplxStr As String = Z.ToStandardString()
             Assert.Equal("-1.125+i5.675", CplxStr)
         End Sub
 
         <Fact>
-        Sub ToStandardString_NegNeg_Succeeds()
+        Sub ToStandardString_DefaultNegNeg_Succeeds()
             Dim Z As New System.Numerics.Complex(-1.125, -5.675)
             Dim CplxStr As String = Z.ToStandardString()
             Assert.Equal("-1.125-i5.675", CplxStr)
