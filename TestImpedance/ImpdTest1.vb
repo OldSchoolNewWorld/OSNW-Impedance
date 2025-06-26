3a45d21b0fad8a3423f9cc78c4494a574e02f630
OldSchoolNewWorld <dreid02893@gmail.com>
2025-06-26 09:56:30 -0400

    Add TestTryParseStandard to Impedance tests.

--- TestImpedance/ImpdTest1.vb
+++ TestImpedance/ImpdTest1.vb
@@ -108,14 +108,103 @@ End Namespace ' TestToStandardString
 
 Namespace TestTryParseStandard
 
-    Public Class PlaceHolder
+    Public Class TryParseStandardDefaultTest
 
-        <Fact>
-        Sub PlaceHolder()
-            'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-            Assert.True(False)
+        <Theory>
+        <InlineData("1.125+i5.675", 1.125, 5.675)>
+        <InlineData("1.125-i5.675", 1.125, -5.675)>
+        <InlineData("-1.125+i5.675", -1.125, 5.675)>
+        <InlineData("-1.125-i5.675", -1.125, -5.675)>
+        Sub TryParseStandard_Default_Succeeds(standardStr As String, real As Double, imaginary As Double)
+            Dim Impd As New OSNW.Numerics.Impedance
+            If Not OSNW.Numerics.Impedance.TryParseStandard(standardStr, Nothing, Nothing, Impd) Then
+                Assert.True(False)
+            End If
+            Assert.True(Impd.Resistance.Equals(real) AndAlso Impd.Reactance.Equals(imaginary))
         End Sub
 
-    End Class ' PlaceHolder
+    End Class ' TryParseStandardDefaultTest
+
+    Public Class TryParseStandardDefaultMixedTest
+
+        <Theory>
+        <InlineData("1.125+i5.675", 1.125, 5.675)> ' A+iB.
+        <InlineData("1.125-5.675i", 1.125, -5.675)> ' A+Bi.
+        <InlineData("-1.125 + i5.675", -1.125, 5.675)> ' Open, one space.
+        <InlineData(" -1.125  -   5.675i  ", -1.125, -5.675)> ' Open, asymmetric spaces.
+        <InlineData("-1.125+ i5.675", -1.125, 5.675)> ' Open, space one side.
+        <InlineData("-1.125 +i5.675", -1.125, 5.675)> ' Open, space one side.
+        <InlineData("1125e-3+i.5675E1", 1.125, 5.675)> ' Exponential notation.
+        Sub TryParseStandard_Default_Succeeds(standardStr As String, real As Double, imaginary As Double)
+            Dim Impd As New OSNW.Numerics.Impedance
+            If Not OSNW.Numerics.Impedance.TryParseStandard(standardStr, Nothing, Nothing, Impd) Then
+                Assert.True(False)
+            End If
+            Assert.True(Impd.Resistance.Equals(real) AndAlso Impd.Reactance.Equals(imaginary))
+        End Sub
+
+    End Class ' TryParseStandardDefaultMixedTest
+
+    Public Class TryParseStandardEnforceStandardizationTest
+
+        Const TightEnforcement As StandardizationStyles =
+            StandardizationStyles.EnforceSequence Or StandardizationStyles.EnforceSpacing
+
+        <Theory>
+        <InlineData("1.125+i5.675", 1.125, 5.675, StandardizationStyles.ClosedAiB)>
+        <InlineData("1.125-5.675i", 1.125, -5.675, StandardizationStyles.ClosedABi)>
+        <InlineData("-1.125 + i5.675", -1.125, 5.675, StandardizationStyles.OpenAiB)>
+        <InlineData("-1.125 - 5.675i", -1.125, -5.675, StandardizationStyles.OpenABi)>
+        Sub TryParseStandard_CompliantStandardization_Succeeds(standardStr As String, real As Double, imaginary As Double,
+                                                       standardizationStyle As StandardizationStyles)
+            Dim Impd As New OSNW.Numerics.Impedance
+            If Not OSNW.Numerics.Impedance.TryParseStandard(standardStr, standardizationStyle, Nothing, Impd) Then
+                Assert.True(False)
+            End If
+            Assert.True(Impd.Resistance.Equals(real) AndAlso Impd.Reactance.Equals(imaginary))
+        End Sub
+
+        <Theory>
+        <InlineData("1.125 + i5.675", StandardizationStyles.ClosedAiB Or TightEnforcement)>
+        <InlineData("1.125-i5.675", StandardizationStyles.ClosedABi Or TightEnforcement)>
+        <InlineData("-1.125+i5.675", StandardizationStyles.OpenAiB Or TightEnforcement)>
+        <InlineData("-1.125 - i5.675", StandardizationStyles.OpenABi Or TightEnforcement)>
+        Sub TryParseStandard_NonCompliantStandardization_Fails(standardStr As String,
+                                                       standardizationStyle As StandardizationStyles)
+            Dim Impd As New OSNW.Numerics.Impedance
+            Assert.False(OSNW.Numerics.Impedance.TryParseStandard(standardStr, standardizationStyle, Nothing, Impd))
+        End Sub
+
+    End Class ' TryParseStandardEnforceStandardizationTest
+
+    Public Class TryParseStandardCultureTest
+
+        <Theory>
+        <InlineData("111111.122-i555555.677", 111_111.122, -555_555.677, 0)> ' One round down, one up.
+        <InlineData("111111.122-i555555.677", 111_111.122, -555_555.677, 1)> ' One round down, one up.
+        <InlineData("1.122+i5.677", 1.122, 5.677, 2)>
+        <InlineData("111111,122-i555555,677", 111_111.122, -555_555.677, 3)> ' One round down, one up.
+        <InlineData("-111111,125+i555555,675", -111_111.125, 555_555.675, 4)>
+        Sub TryParseStandard_Culture_Succeeds(standardStr As String, real As Double, imaginary As Double,
+                                     index As Integer)
+
+            Dim Providers As System.IFormatProvider() = {
+                CultureInfo.InvariantCulture,
+                CultureInfo.CurrentCulture,
+                New CultureInfo("en-UK", False),
+                New CultureInfo("ru-RU", False),
+                New CultureInfo("fr-FR", False)
+            }
+            Dim Impd As New OSNW.Numerics.Impedance
+
+            If Not OSNW.Numerics.Impedance.TryParseStandard(standardStr, Nothing, Providers(index), Impd) Then
+                Assert.True(False)
+            End If
+
+            Assert.True(Impd.Resistance.Equals(real) AndAlso Impd.Reactance.Equals(imaginary))
+
+        End Sub
+
+    End Class ' TryParseStandardCultureTest
 
 End Namespace ' TestTryParseStandard
