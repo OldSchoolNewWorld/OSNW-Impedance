d1491f11f25ea6f4a9435382495e8e115e4cbe13
OldSchoolNewWorld <dreid02893@gmail.com>
2025-08-19 06:47:44 -0400

    Implement TestGetRadiusG.

--- TestImpedance/ImpGeoTest1.vb
+++ TestImpedance/ImpGeoTest1.vb
@@ -26,10 +26,10 @@ Namespace GeometryTests
         End Sub
 
         <Theory>
-        <InlineData(4.0, 8.0, 4.0, 1.0, 0.0, 0.5)>
-        <InlineData(4.0, 8.0, 4.0, 1.0, -2.0, 2.0 / 3.0)>
+        <InlineData(4.0, 8.0, 4.0, 1.0, 0.0)>
+        <InlineData(4.0, 8.0, 4.0, 1.0, -2.0)>
         Sub GetRadiusR_BadInput_Fails(gridCenterX As Double, gridCenterY As Double, gridDiameter As Double,
-                                      z0 As Double, testRes As Double, expectRad As Double)
+                                      z0 As Double, testRes As Double)
 
             Dim SmithCirc As New SmithMainCircle(gridCenterX, gridCenterY, gridDiameter, z0)
             Dim Ex As Exception = Assert.Throws(Of ArgumentOutOfRangeException)(
@@ -44,19 +44,16 @@ Namespace GeometryTests
     Public Class TestGetRadiusX
 
         <Theory>
-        <InlineData(4.0, 8.0, 4.0, 1.0, 3.0, 2.0 / 3.0)> ' Meas
-        <InlineData(4.0, 8.0, 4.0, 50.0, 100.0, 1.0)> ' Meas
-        <InlineData(4.0, 8.0, 4.0, 1.0, 1.0, 2.0)> ' Meas
-        <InlineData(4.0, 8.0, 4.0, 1.0, 0.5, 4.0)> ' Meas
-        <InlineData(4.0, 8.0, 4.0, 1.0, 1.0 / 3.0, 6.0)> ' Meas
-        <InlineData(4.0, 8.0, 4.0, 1.0, 0.5, 4.0)> ' Est
-        <InlineData(4.0, 8.0, 4.0, 75.0, 25.0, 6.0)>
-        <InlineData(4.0, 8.0, 4.0, 1.0, -0.5, 4.0)> ' Est
+        <InlineData(4.0, 8.0, 4.0, 1.0, 3.0, 2.0 / 3.0)>
+        <InlineData(4.0, 8.0, 4.0, 50.0, 100.0, 1.0)>
+        <InlineData(4.0, 8.0, 4.0, 1.0, 1.0, 2.0)>
+        <InlineData(4.0, 8.0, 4.0, 1.0, 0.5, 4.0)>
+        <InlineData(4.0, 8.0, 4.0, 1.0, 1.0 / 3.0, 6.0)>
         <InlineData(4.0, 8.0, 4.0, 75.0, -25.0, 6.0)>
-        <InlineData(4.0, 8.0, 4.0, 1.0, -1.0, 2.0)> ' Meas
-        <InlineData(4.0, 8.0, 4.0, 1.0, -1.0, 2.0)> ' Meas
-        <InlineData(4.0, 8.0, 4.0, 50.0, -100.0, 1.0)> ' Meas
-        <InlineData(4.0, 8.0, 4.0, 1.0, -3.0, 2.0 / 3.0)> ' Meas
+        <InlineData(4.0, 8.0, 4.0, 50.0, -25.0, 4.0)>
+        <InlineData(4.0, 8.0, 4.0, 1.0, -1.0, 2.0)>
+        <InlineData(4.0, 8.0, 4.0, 50.0, -100.0, 1.0)>
+        <InlineData(4.0, 8.0, 4.0, 1.0, -3.0, 2.0 / 3.0)>
         Sub GetRadiusX_GoodInput_Succeeds(gridCenterX As Double, gridCenterY As Double, gridDiameter As Double,
                                           z0 As Double, testReact As Double, expectRad As Double)
 
@@ -66,9 +63,9 @@ Namespace GeometryTests
         End Sub
 
         <Theory>
-        <InlineData(4.0, 8.0, 4.0, 1.0, 0.0, 0.5)>
+        <InlineData(4.0, 8.0, 4.0, 1.0, 0.0)>
         Sub GetRadiusX_BadInput_Fails(gridCenterX As Double, gridCenterY As Double, gridDiameter As Double,
-                                      z0 As Double, testRes As Double, expectRad As Double)
+                                      z0 As Double, testRes As Double)
 
             Dim SmithCirc As New SmithMainCircle(gridCenterX, gridCenterY, gridDiameter, z0)
             Dim Ex As Exception = Assert.Throws(Of ArgumentOutOfRangeException)(
@@ -84,34 +81,78 @@ Namespace GeometryTests
 
         <Theory>
         <InlineData(4.0, 8.0, 4.0, 1.0, 3.0, 0.5)>
-        <InlineData(4.0, 8.0, 4.0, 50.0, 100.0, 2.0 / 3.0)>
+        <InlineData(4.0, 8.0, 4.0, 50.0, 2.0 * (1.0 / 50.0), 2.0 / 3.0)>
         <InlineData(4.0, 8.0, 4.0, 1.0, 1.0, 1.0)>
         <InlineData(4.0, 8.0, 4.0, 1.0, 0.5, 4.0 / 3.0)>
-        <InlineData(4.0, 8.0, 4.0, 75.0, 25.0, 1.5)>
-        Sub GetRadiusG_Succeeds(gridCenterX As Double, gridCenterY As Double, gridDiameter As Double,
-                                z0 As Double, testCond As Double, expectRad As Double)
+        <InlineData(4.0, 8.0, 4.0, 75.0, (1.0 / 75.0) / 3.0, 1.5)>
+        Sub GetRadiusG_GoodInput_Succeeds(gridCenterX As Double, gridCenterY As Double, gridDiameter As Double,
+                                          z0 As Double, testCond As Double, expectRad As Double)
 
             Dim SmithCirc As New SmithMainCircle(gridCenterX, gridCenterY, gridDiameter, z0)
             Dim RadiusAns As Double = SmithCirc.GetRadiusG(testCond)
             Assert.Equal(expectRad, RadiusAns)
         End Sub
 
+        <Theory>
+        <InlineData(4.0, 8.0, 4.0, 1.0, 0.0)>
+        <InlineData(4.0, 8.0, 4.0, 1.0, -2.0)>
+        Sub GetRadiusG_BadInput_Fails(gridCenterX As Double, gridCenterY As Double, gridDiameter As Double,
+                                      z0 As Double, testCond As Double)
+
+            Dim SmithCirc As New SmithMainCircle(gridCenterX, gridCenterY, gridDiameter, z0)
+            Dim Ex As Exception = Assert.Throws(Of ArgumentOutOfRangeException)(
+                Sub()
+                    ' Code that throws the exception
+                    Dim RadiusAns As Double = SmithCirc.GetRadiusG(testCond)
+                End Sub)
+        End Sub
+
     End Class ' TestGetRadiusG
 
     Public Class TestGetRadiusB
 
+        '<Theory>
+        '<InlineData(4.0, 8.0, 4.0, 1.0, 3.0, 2.0 / 3.0)>
+        '<InlineData(4.0, 8.0, 4.0, 50.0, 2.0 / 50.0, 1.0)>
+        '<InlineData(4.0, 8.0, 4.0, 1.0, 1.0, 2.0)>
+        '<InlineData(4.0, 8.0, 4.0, 1.0, 0.5, 4.0)>
+        '<InlineData(4.0, 8.0, 4.0, 1.0, 1.0 / 3.0, 6.0)>
+        '<InlineData(4.0, 8.0, 4.0, 75.0, -1 / (3.0 * 75.0), 6.0)>
+        '<InlineData(4.0, 8.0, 4.0, 50.0, -1 / (2.0 * 50.0), 4.0)>
+        '<InlineData(4.0, 8.0, 4.0, 1.0, -1.0, 2.0)>
+        '<InlineData(4.0, 8.0, 4.0, 50.0, -2.0 / 50.0, 1.0)>
+        '<InlineData(4.0, 8.0, 4.0, 1.0, -3.0, 2.0 / 3.0)>
+
         <Theory>
-        <InlineData(4.0, 8.0, 4.0, 1.0, 3.0, 0.5)>
-        <InlineData(4.0, 8.0, 4.0, 1.0, -2.0, 2.0 / 3.0)>
-        <InlineData(4.0, 8.0, 4.0, 1.0, 1.0, 1.0)>
-        <InlineData(4.0, 8.0, 4.0, 1.0, -1 / 2.0, 4.0 / 3.0)>
-        <InlineData(4.0, 8.0, 4.0, 1.0, 1 / 3.0, 1.5)>
-        Sub GetRadiusB_Succeeds(gridCenterX As Double, gridCenterY As Double, gridDiameter As Double,
-                                z0 As Double, testCond As Double, expectRad As Double)
+        <InlineData(4.0, 8.0, 4.0, 1.0, 3.0, 2.0 / 3.0)>
+        <InlineData(4.0, 8.0, 4.0, 50.0, 100.0, 1.0)>
+        <InlineData(4.0, 8.0, 4.0, 1.0, 1.0, 2.0)>
+        <InlineData(4.0, 8.0, 4.0, 1.0, 0.5, 4.0)>
+        <InlineData(4.0, 8.0, 4.0, 1.0, 1.0 / 3.0, 6.0)>
+        <InlineData(4.0, 8.0, 4.0, 75.0, -25.0, 6.0)>
+        <InlineData(4.0, 8.0, 4.0, 50.0, -25.0, 4.0)>
+        <InlineData(4.0, 8.0, 4.0, 1.0, -1.0, 2.0)>
+        <InlineData(4.0, 8.0, 4.0, 50.0, -100.0, 1.0)>
+        <InlineData(4.0, 8.0, 4.0, 1.0, -3.0, 2.0 / 3.0)>
+        Sub GetRadiusB_GoodInput_Succeeds(gridCenterX As Double, gridCenterY As Double, gridDiameter As Double,
+                                z0 As Double, testSusc As Double, expectRad As Double)
 
             Dim SmithCirc As New SmithMainCircle(gridCenterX, gridCenterY, gridDiameter, z0)
-            Dim RadiusAns As Double = SmithCirc.GetRadiusB(testCond)
+            Dim RadiusAns As Double = SmithCirc.GetRadiusB(testSusc)
             Assert.Equal(expectRad, RadiusAns)
+        End Sub
+
+        <Theory>
+        <InlineData(4.0, 8.0, 4.0, 1.0, 0.0)>
+        Sub GetRadiusB_BadInput_Fails(gridCenterX As Double, gridCenterY As Double, gridDiameter As Double,
+                                      z0 As Double, testSusc As Double)
+
+            Dim SmithCirc As New SmithMainCircle(gridCenterX, gridCenterY, gridDiameter, z0)
+            Dim Ex As Exception = Assert.Throws(Of ArgumentOutOfRangeException)(
+                Sub()
+                    ' Code that throws the exception
+                    Dim RadiusAns As Double = SmithCirc.GetRadiusB(testSusc)
+                End Sub)
         End Sub
 
     End Class ' TestGetRadiusB
@@ -132,11 +173,11 @@ Namespace GeometryTests
         End Sub
 
         <Theory>
-        <InlineData(4.0, 8.0, 4.0, 1.0, -1.0, 1.5)>
-        <InlineData(4.0, 8.0, 4.0, 1.0, 1 / 2.0, 4.0 / 3.0)>
-        <InlineData(4.0, 8.0, 4.0, 1.0, 1.0, 1.0)>
+        <InlineData(4.0, 8.0, 4.0, 1.0, -1.0)>
+        <InlineData(4.0, 8.0, 4.0, 1.0, 1 / 2.0)>
+        <InlineData(4.0, 8.0, 4.0, 1.0, 1.0)>
         Sub GetRadiusV_BadInput_Fails(gridCenterX As Double, gridCenterY As Double, gridDiameter As Double,
-                                          z0 As Double, testV As Double, expectRad As Double)
+                                          z0 As Double, testV As Double)
 
             Dim SmithCirc As New SmithMainCircle(gridCenterX, gridCenterY, gridDiameter, z0)
             Dim Ex As Exception = Assert.Throws(Of ArgumentOutOfRangeException)(
