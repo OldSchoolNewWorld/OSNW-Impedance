7da802e94f393d15832c12e951816f0c2c4563ec
OldSchoolNewWorld <dreid02893@gmail.com>
2025-11-30 10:26:36 -0500

    Clean up arbitrary match tests.

--- TestImpedance/ImpdMatchTest.vb
+++ TestImpedance/ImpdMatchTest.vb
@@ -393,85 +393,11 @@ Namespace MatchArbitraryTests
         <InlineData(50.0, 10.0, 70.0)> ' O50: In the top center. Z0=50.
         <InlineData(1.0, 0.4, -0.8)> ' P1: In the bottom center.
         <InlineData(50.0, 20.0, -40.0)> ' P50: In the bottom center. Z0=50.
-        Public Sub MatchArbitrary_Z0_Succeeds(z0 As System.Double, loadR As Double, loadX As Double)
+        Public Sub MatchArbitraryZ0_GoodInput_Succeeds(z0 As System.Double, loadR As Double, loadX As Double)
 
             Dim LoadZ As New Impedance(loadR, loadX)
             Dim SourceZ As New Impedance(z0, 0.0)
             Dim Transformations As Transformation() = Array.Empty(Of Transformation)
-
-            Assert.True(Impedance.MatchArbitrary(z0, LoadZ, SourceZ, Transformations))
-
-        End Sub
-
-        '<InlineData(1.0, INF, 0.0000, "C: At the open circuit point on the right.")>
-        '<InlineData(1.0, 0.0000, 0.0000, "A: At the short circuit point. Omit - covered by B.")>
-        '<InlineData(1.0, 0.0000, 1 / 2.0, "B: Anywhere else on the perimeter. R=0.0.")>
-        '<InlineData(1.0, -2.0, 999, "R: NormR<=0. Invalid.")>
-        '<InlineData(1.0, -0.0345, 0.4138, "Q: Outside of main circle. Invalid.")>
-        '<InlineData(  Z0,        R,         X, "Model")>
-        <Theory>
-        <InlineData(1.0, 1.0, 0.0000, "D1: At the center.")>
-        <InlineData(75.0, 75.0, 0.0000, "D75: At the center. Z0=75.")>
-        <InlineData(1.0, 1.0, 1.0, "E1: On R=Z0 circle, above resonance line. Only needs reactance.")>
-        <InlineData(50.0, 50.0, 50.0, "E50: On R=Z0 circle, above resonance line. Only needs reactance. Z0=50.")>
-        <InlineData(1.0, 1.0, -2.0, "F1: On R=Z0 circle, below resonance line. Only needs reactance.")>
-        <InlineData(50.0, 50.0, -100.0, "F50: On R=Z0 circle, below resonance line. Only needs reactance. Z0=50.")>
-        <InlineData(1.0, 2.0, 1 / 2.0, "G1: Inside R=Z0 circle, above resonance line.")>
-        <InlineData(50.0, 100.0, 25.0, "G50: Inside R=Z0 circle, above resonance line. Z0=50.")>
-        <InlineData(1.0, 3.0, 0.0000, "H1: Inside R=Z0 circle, on line.")>
-        <InlineData(50.0, 150.0, 0.0000, "H50: Inside R=Z0 circle, on line. Z0=50.")>
-        <InlineData(1.0, 2.0, -2.0, "I1: Inside R=Z0 circle, below resonance line.")>
-        <InlineData(50.0, 100.0, -100.0, "I50: Inside R=Z0 circle, below resonance line. Z0=50.")>
-        <InlineData(1.0, 1 / 2.0, 1 / 2.0, "J1: On G=Y0 circle, above resonance line. Only needs reactance.")>
-        <InlineData(50.0, 25.0, 25.0, "J50: On G=Y0 circle, above resonance line. Only needs reactance. Z0=50.")>
-        <InlineData(1.0, 1 / 2.0, -1 / 2.0, "K1: On G=Y0 circle, below resonance line. Only needs reactance.")>
-        <InlineData(50.0, 25.0, -25.0, "K50: On G=Y0 circle, below resonance line. Only needs reactance. Z0=50.")>
-        <InlineData(1.0, 1 / 3.0, 1 / 3.0, "L1: Inside G=Y0 circle, above resonance line.")>
-        <InlineData(75.0, 25.0, 25.0, "L75: Inside G=Y0 circle, above resonance line. Z0=75.")>
-        <InlineData(1.0, 1 / 3.0, 0.0000, "M1: Inside G=Y0 circle, on line.")>
-        <InlineData(75.0, 25.0, 0.0000, "M75: Inside G=Y0 circle, on line. Z0=75.")>
-        <InlineData(1.0, 1 / 2.0, -1 / 3.0, "N1: Inside G=Y0 circle, below line.")>
-        <InlineData(75.0, 37.5, -25.0, "N75: Inside G=Y0 circle, below line. Z0=75.")>
-        <InlineData(1.0, 0.2, 1.4, "O1: In the top center.")>
-        <InlineData(50.0, 10.0, 70.0, "O50: In the top center. Z0=50.")>
-        <InlineData(1.0, 0.4, -0.8, "P1: In the bottom center.")>
-        <InlineData(50.0, 20.0, -40.0, "P50: In the bottom center. Z0=50.")>
-        Public Sub MatchArbitraryZ0_GoodInput_Succeeds(z0 As System.Double, loadR As Double, loadX As Double,
-                                                       name As String)
-
-            Dim LoadZ As New Impedance(loadR, loadX)
-            Dim SourceZ As New Impedance(z0, 0.0)
-            Dim Transformations As Transformation() = Array.Empty(Of Transformation)
-
-            Impedance.MatchArbitrary(z0, LoadZ, SourceZ, Transformations)
-
-
-            'xxxxxxxxxxxxx
-
-            '        ' Use Debug|Windows|Immediate to see the output.
-            '        ' That only updates when debugging.
-
-            Dim RowStr As New StringBuilder(1000)
-            Dim OutStr As String
-
-            OutStr = String.Format("{0}", name)
-            RowStr.AppendLine(OutStr)
-
-            OutStr = String.Format("Z0={0}, LoadZ={1}", z0, LoadZ.ToString)
-            RowStr.AppendLine(OutStr)
-
-            If Transformations.Length.Equals(0) Then
-                OutStr = "No solution."
-                RowStr.AppendLine(OutStr)
-            Else
-                For Each OneTrans As Transformation In Transformations
-                    OutStr = String.Format("{0}, Values={1}; {2}; {3}", OneTrans.Style, OneTrans.Value1, OneTrans.Value2, OneTrans.Value3)
-                    RowStr.AppendLine(OutStr)
-                Next
-            End If
-            RowStr.AppendLine(String.Empty)
-
-            System.Diagnostics.Debug.Write($"{RowStr}")
 
             Assert.True(Impedance.MatchArbitrary(z0, LoadZ, SourceZ, Transformations))
 
@@ -482,28 +408,28 @@ Namespace MatchArbitraryTests
     Public Class TestMatchArbitraryAny
 
         <Theory>
-        <InlineData(1, 1.0, 1.0, 0.5, 0.2, "L1 to AMRIS1.")> ' 
-        <InlineData(1, 0.5, 0.2, 1.0, 1.0, "AMRIS1 to L1.")> ' 
-        <InlineData(100.0, 100.0, 100.0, 50.0, 20.0, "AMRIS100.")> ' 
-        <InlineData(100.0, 50.0, 20.0, 100.0, 100.0, "AMRIS100 reverse.")> ' 
-        <InlineData(1, 1.0, 1.0, 2.0, -2.0, "E1 to I1.")> ' 
-        <InlineData(1, 2.0, -2.0, 1.0, 1.0, "I1 to E1.")> ' 
-        <InlineData(75, 50.0, 50.0, 100.0, -100.0, "E50 to I50.")> ' 
-        <InlineData(75, 100.0, -100.0, 50.0, 50.0, "I50 to E50.")> ' 
-        <InlineData(1, 1 / 3.0, 1 / 3.0, 1 / 3.0, 0.0000, "L1 to M1.")> ' 
-        <InlineData(1, 1 / 3.0, 0.0000, 1 / 3.0, 1 / 3.0, "M1 to L1.")> ' 
-        <InlineData(75, 25.0, 25.0, 25.0, 0.0000, "L75 to M75.")> ' 
-        <InlineData(75, 25.0, 0.0000, 25.0, 25.0, "M75 to L75.")> ' 
-        <InlineData(1.0, 0.2, 1.4, 0.4, -0.8, "O1 to P1.")> ' 
-        <InlineData(1.0, 0.4, -0.8, 0.2, 1.4, "P1 to O1.")> ' 
-        <InlineData(50.0, 10.0, 70.0, 20.0, -40.0, "O50 to P50.")> ' 
-        <InlineData(50.0, 20.0, -40.0, 10.0, 70.0, "P50 to O50.")> ' 
-        <InlineData(1.0, 1.0, 1.0, 1 / 2.0, 1 / 2.0, "E1 to J1.")> ' 
-        <InlineData(1.0, 1 / 2.0, 1 / 2.0, 1.0, 1.0, "J1 to E1.")> ' 
-        <InlineData(50.0, 50.0, 50.0, 25.0, 25.0, "E50 to J50.")> ' 
-        <InlineData(50.0, 25.0, 25.0, 50.0, 50.0, "J50 to E50.")> ' 
-        Public Sub MatchArbitrary_Succeeds(z0 As Double, loadR As Double, loadX As Double, sourceR As Double,
-                                           sourceX As Double, name As String)
+        <InlineData(1, 1.0, 1.0, 0.5, 0.2)> ' 
+        <InlineData(1, 0.5, 0.2, 1.0, 1.0)> ' 
+        <InlineData(100.0, 100.0, 100.0, 50.0, 20.0)> ' 
+        <InlineData(100.0, 50.0, 20.0, 100.0, 100.0)> ' 
+        <InlineData(1, 1.0, 1.0, 2.0, -2.0)> ' 
+        <InlineData(1, 2.0, -2.0, 1.0, 1.0)> ' 
+        <InlineData(75, 50.0, 50.0, 100.0, -100.0)> ' 
+        <InlineData(75, 100.0, -100.0, 50.0, 50.0)> ' 
+        <InlineData(1, 1 / 3.0, 1 / 3.0, 1 / 3.0, 0.0000)> ' 
+        <InlineData(1, 1 / 3.0, 0.0000, 1 / 3.0, 1 / 3.0)> ' 
+        <InlineData(75, 25.0, 25.0, 25.0, 0.0000)> ' 
+        <InlineData(75, 25.0, 0.0000, 25.0, 25.0)> ' 
+        <InlineData(1.0, 0.2, 1.4, 0.4, -0.8)> ' 
+        <InlineData(1.0, 0.4, -0.8, 0.2, 1.4)> ' 
+        <InlineData(50.0, 10.0, 70.0, 20.0, -40.0)> ' 
+        <InlineData(50.0, 20.0, -40.0, 10.0, 70.0)> ' 
+        <InlineData(1.0, 1.0, 1.0, 1 / 2.0, 1 / 2.0)> ' 
+        <InlineData(1.0, 1 / 2.0, 1 / 2.0, 1.0, 1.0)> ' 
+        <InlineData(50.0, 50.0, 50.0, 25.0, 25.0)> ' 
+        <InlineData(50.0, 25.0, 25.0, 50.0, 50.0)> ' 
+        Public Sub MatchArbitrary_GoodInput_Succeeds(z0 As Double, loadR As Double, loadX As Double,
+                                                     sourceR As Double, sourceX As Double)
 
             Dim LoadZ As New Impedance(loadR, loadX)
             Dim SourceZ As New Impedance(sourceR, sourceX)
